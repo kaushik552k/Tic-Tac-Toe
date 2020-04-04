aa3d05b31f00f90d920447d1f67ba8d7090200b9
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-04-04 14:06:10 +0530

    created a method user is a gamemaster

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -6,7 +6,13 @@ public class Main {
     public static void main(String[] args){
         // write your code here
         Scanner sc = new Scanner( System.in );
+        System.out.println( "Enter cells" );
         char[] input = sc.nextLine().toCharArray();
+        System.out.println( "---------" );
+        System.out.println( "| " + input[0] + " " + input[1] + " " + input[2] + " |" );
+        System.out.println( "| " + input[3] + " " + input[4] + " " + input[5] + " |" );
+        System.out.println( "| " + input[6] + " " + input[7] + " " + input[8] + " |" );
+        System.out.println( "---------" );
 
     }
 }
