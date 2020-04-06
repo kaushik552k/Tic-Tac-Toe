5f8fab954e74b85f6397b82a59c12a80b24b8e2e
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-04-06 13:17:38 +0530

    Scanner class Object to take the user input

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -3,16 +3,106 @@ package tictactoe;
 import java.util.Scanner;
 
 public class Main {
+    final static Scanner scanner = new Scanner( System.in );
+
+    public static int countSymbols(char symbol, char[] arrayTicTac){
+        int howManyTimes = 0;
+        for (int i = 0; i < arrayTicTac.length; i++) {
+            if(arrayTicTac[i] == symbol) {
+                howManyTimes++;
+            }
+        }
+        return howManyTimes;
+    }
+
+
+    public static boolean checkRow(char symbol, char[][] symbolArray){
+
+        for (int row = 0; row < 3; row++) {
+            if((symbol == symbolArray[row][0]) && (symbol == symbolArray[row][1]) && (symbol == symbolArray[row][2])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkColumn(char symbol, char[][] symbolArray){
+        for (int column = 0; column < 3; column++) {
+            if((symbol == symbolArray[0][column]) && (symbol == symbolArray[1][column]) && (symbol == symbolArray[2][column])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkDiagonal(char symbol, char[][] symbolArray){
+
+        return ((symbol == symbolArray[0][0]) && (symbol == symbolArray[1][1]) && (symbol == symbolArray[2][2])) ||
+                ((symbol == symbolArray[2][0]) && (symbol == symbolArray[1][1]) && (symbol == symbolArray[0][2]));
+    }
+
+    public static boolean xWins(char[][] symbolArray){
+        return checkColumn( 'X', symbolArray ) || checkRow( 'X', symbolArray ) || checkDiagonal( 'X', symbolArray );
+    }
+
+    public static boolean oWins(char[][] symbolArray){
+        return (checkColumn( 'O', symbolArray ) || checkRow( 'O', symbolArray ) || checkDiagonal( 'O', symbolArray ));
+    }
+
+    public static char[][] translateTableToMatrix(int tableLenght, char[] tabToTranslate){
+
+        int sqrtedTableLenght = (int) Math.sqrt( tableLenght );
+        char[][] newMatrix = new char[sqrtedTableLenght][sqrtedTableLenght];
+
+        int tabIterator = 0;
+
+        for (int i = 0; i < sqrtedTableLenght; i++) {
+            for (int j = 0; j < sqrtedTableLenght; j++) {
+
+                newMatrix[i][j] = tabToTranslate[tabIterator];
+                tabIterator++;
+            }
+        }
+        return newMatrix;
+    }
+
+
     public static void main(String[] args){
-        // write your code here
-        Scanner sc = new Scanner( System.in );
-        System.out.println( "Enter cells" );
-        char[] input = sc.nextLine().toCharArray();
+
+        char[][] gameTable;
+
+        char[] ticTac = scanner.nextLine().toCharArray();
+
+        gameTable = translateTableToMatrix( ticTac.length, ticTac );
+
         System.out.println( "---------" );
-        System.out.println( "| " + input[0] + " " + input[1] + " " + input[2] + " |" );
-        System.out.println( "| " + input[3] + " " + input[4] + " " + input[5] + " |" );
-        System.out.println( "| " + input[6] + " " + input[7] + " " + input[8] + " |" );
+        for (int rows = 0; rows < 3; rows++) {
+            for (int columns = 0; columns < 3; columns++) {
+
+                if(columns == 0)
+                    System.out.print( "| " );
+
+                System.out.print( gameTable[rows][columns] + " " );
+
+                if(columns == 2)
+                    System.out.print( "|" );
+            }
+            System.out.println();
+        }
         System.out.println( "---------" );
 
+        int numberOfXs = countSymbols( 'X', ticTac );
+        int numberOfOs = countSymbols( 'O', ticTac );
+        int numberOf_s = countSymbols( '_', ticTac );
+
+        boolean isXaWinner = xWins( gameTable );
+        boolean isOaWinner = oWins( gameTable );
+
+        if((isOaWinner && isXaWinner) || (Math.abs( numberOfXs - numberOfOs )) >= 2) System.out.println( "Impossible" );
+
+        else if(isOaWinner) System.out.println( "O wins" );
+        else if(isXaWinner) System.out.println( "X wins" );
+        else if(numberOf_s > 0 && !isOaWinner && !isXaWinner) System.out.println( "Game not finished" );
+        else if(numberOf_s == 0 && !isOaWinner && !isXaWinner) System.out.println( "Draw" );
     }
 }
